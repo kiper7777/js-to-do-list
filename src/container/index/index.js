64c284e371f07ed3b8883b94a16fae8c3c5387b4
Dmytro Kiperman <8boyss@ukr.net>
2023-11-21 23:54:15 +0100

    first commit

--- src/container/index/index.js
+++ src/container/index/index.js
@@ -1,3 +1,16 @@
-import '../../script/test'
+export class Todo {
+  static #block = null
+  static #template = null
+  static #input = null
+  static #button = null
 
-console.log('container')
+  static init = () => {
+    this.#template = document.getElementById('task')
+
+    console.log(this.#template.content.firstElementChild)
+  }
+}
+
+Todo.init()
+
+window.todo = Todo
