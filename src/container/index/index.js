c87f84edf4536c08fec0d2661b93eaa2a7bddb31
Dmytro Kiperman <8boyss@ukr.net>
2023-11-24 23:16:43 +0100

    create task js-to-do-list

--- src/container/index/index.js
+++ src/container/index/index.js
@@ -1,4 +1,26 @@
 export class Todo {
+  static #NAME = 'todo'
+
+  static #saveData = () => {
+    localStorage.setItem(
+      this.#NAME,
+      JSON.stringify({
+        list: this.#list,
+        count: this.#count,
+      }),
+    )
+  }
+
+  static #loadData = () => {
+    const data = localStorage.getItem(this.#NAME)
+
+    if (data) {
+      const { list, count } = JSON.parse(data)
+      this.#list = list
+      this.#count = count
+    }
+  }
+
   static #list = []
   static #count = 0
 
@@ -24,9 +46,15 @@ export class Todo {
     // console.log(this.#template)
 
     this.#block = document.querySelector('.task__list')
+
     this.#input = document.querySelector('.form__input')
+
     this.#button = document.querySelector('.form__button')
+
     this.#button.onclick = this.#handleAdd
+
+    this.#loadData()
+
     this.#render()
 
     // console.log(
@@ -38,9 +66,13 @@ export class Todo {
   }
 
   static #handleAdd = () => {
-    this.#createTaskData(this.#input.value)
-    this.#input.value = ''
-    this.#render()
+    const value = this.#input.value
+    if (value.length > 1) {
+      this.#createTaskData(value)
+      this.#input.value = ''
+      this.#render()
+      this.#saveData()
+    }
     // console.log(this.#list)
   }
 
@@ -51,7 +83,7 @@ export class Todo {
       this.#block.innerText = `Список задач пустий`
     } else {
       this.#list.forEach((taskData) => {
-        const el = this.#createTaskElem.cloneNode(taskData)
+        const el = this.#createTaskElem(taskData)
         this.#block.append(el)
       })
     }
@@ -65,7 +97,55 @@ export class Todo {
     id.innerText = `${data.id}.`
     text.innerText = data.text
 
+    btnCancel.onclick = this.#handleCancel(data)
+
+    btnDo.onclick = this.#handleDo(data, btnDo, el)
+
+    if (data.done) {
+      el.classList.add('task--done')
+      btnDo.classList.remove('task__button--do')
+      btnDo.classList.add('task__button--done')
+    }
+
     return el
+  }
+
+  static #handleDo = (data, btn, el) => () => {
+    const result = this.#toggleDone(data.id)
+
+    if (result === true || result === false) {
+      el.classList.toggle('task--done')
+      btn.classList.toggle('task__button--do')
+      btn.classList.toggle('task__button--done')
+
+      this.#saveData()
+    }
+  }
+
+  static #toggleDone = (id) => {
+    const task = this.#list.find((item) => item.id === id)
+
+    if (task) {
+      task.done = !task.done
+      return task.done
+    } else {
+      return null
+    }
+  }
+
+  static #handleCancel = (data) => () => {
+    if (confirm('Видалити задачу?')) {
+      const result = this.#deleteById(data.id)
+      if (result) {
+        this.#render()
+        this.#saveData()
+      }
+    }
+  }
+
+  static #deleteById = (id) => {
+    this.#list = this.#list.filter((item) => item.id !== id)
+    return true
   }
 }
 
